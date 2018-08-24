0effe23e9f6a4f8aa7bd7cc0b579c1ae9d2ba610
Héricles Emanuel <hericles.me@gmail.com>
2018-08-24 20:51:26 -0300

    add _config

--- frontend/app/components/portfolio/portfolioController.js
+++ frontend/app/components/portfolio/portfolioController.js
@@ -1,4 +1,38 @@
 angular.module("portfolio", [])
-    .controller("portfolioCtrl", function($scope) {
+    .controller("portfolioCtrl", function ($scope) {
+        
+        $scope.page = {
+            title: 'CodeX',
+            subtitle: 'Empresa Jr. de Computação UFCG'
+        };
+
+        $scope.header = {
+            title: "Nosso Portfolio",
+            subtitle: "Acompanhe as soluções desenvolvidas pela nossa equipe"
+        }
+
+        $scope.classes = {
+            subtitle: "section-heading-upper",
+            title: "section-heading-lower"
+        }
+
+        $scope.cards = [
+            {
+                nome: "Produto 1", 
+                site: "http://sitedoproduto1.com",
+                descricao: "Lorem ipsum dolor sit amet, consectetur \
+                    adipisicing elit, sed do eiusmod tempor incididunt \
+                    ut labore et.",
+                image: "https://mestredoadwords.com.br/wp-content/uploads/2016/12/vender-produtos.png"  
+            },
+            {
+                nome: "Produto 2", 
+                site: "http://sitedoproduto2.com",
+                descricao: "Lorem ipsum dolor sit amet, consectetur \
+                    adipisicing elit, sed do eiusmod tempor incididunt \
+                    ut labore et.",
+                image: "https://image.dhgate.com/0x0/f2/albu/g5/M01/98/E1/rBVaI1jCGfyAQ1eSAABuzuzKv9w821.jpg"  
+            }
+        ]
 
     });
